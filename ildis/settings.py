0fe81377847ca164c03509ceb96ccb6f2d870e1b
Jochem Raat <jochem@invulns.nl>
2021-11-08 23:17:30 +0000

    Add bootstrap

--- ildis/settings.py
+++ ildis/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
     'ilcon',
     'iltext',
 ]
@@ -74,7 +75,7 @@ LOGGING = {
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
