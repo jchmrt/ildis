5b8895bbc44b2eb9a107b4e3a4b5a7bdc593e2d7
Jochem Raat <jochem@invulns.nl>
2021-11-08 23:49:32 +0100

    Move secret key to separate file

--- ildis/settings.py
+++ ildis/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qf3ascetbr92##tkb_$d1r$-5t&052zekh%k97vc2c2d)qmm$5'
+with open(str(BASE_DIR) + '/SECRET_KEY') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
