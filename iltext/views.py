ad22c1ba94c821e433ac05ecf9e1bdc2c1894508
Jochem Raat <jochem@invulns.nl>
2021-12-06 18:51:52 +0000

    Make login required for iltext

--- iltext/views.py
+++ iltext/views.py
@@ -2,19 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.apps import apps
+from django.contrib.auth.decorators import login_required
 
 from iltext.iltext import Iltext
 from ilcon.waves import Waves
 
-# Create your views here.
+@login_required
 def index(request):
     return render(request, 'iltext/index.html', {})
 
+@login_required
 def send(request):
-    iltext = Iltext(request.POST['text'])
+    iltext = Iltext("              " + request.POST['text'])
     ilcon = apps.get_app_config('ilcon').ilcon
 
-    ilcon.send(iltext)
+    if request.POST['text'] == "":
+        ilcon.send(Waves())
+    else:
+        ilcon.send(iltext)
 
     return HttpResponseRedirect(reverse('iltext:index'))
 
