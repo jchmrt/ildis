fbfd13026d2478b3f9066e557079c07312333fc3
Jochem Raat <jochem@invulns.nl>
2022-04-08 23:10:11 +0200

    Better snake version

---
+++ snake/snake_2.py
@@ -0,0 +1,315 @@
+import threading
+import random
+from enum import Enum
+
+from ildis.ildis import Disp
+
+class Direction(Enum):
+    NORTH = 1
+    SOUTH = 2
+    WEST = 3
+    EAST = 4
+
+    def is_irrelevant(self, other):
+        if self is Direction.NORTH or self is Direction.SOUTH:
+            return other is Direction.NORTH or other is Direction.SOUTH
+        else:
+            return other is Direction.WEST or other is Direction.EAST
+
+
+
+class Tile:
+    def __init__(self):
+        self.snake_1 = None
+        self.snake_2 = None
+        self.food = False
+        self.lock = threading.RLock()
+
+    def completely_empty(self):
+        return (not self.snake_1) and (not self.snake_2) and (not self.food)
+
+    def occupied(self):
+        with self.lock:
+            return ((self.snake_1) or
+                    (self.snake_2))
+
+    def move_to_spot(self, snake):
+        with self.lock:
+            if self.occupied():
+                return False
+            else:
+                self.snake_1 = snake
+                return True
+
+    def leave_spot(self, snake):
+        with self.lock:
+            if self.snake_1 == snake:
+                self.snake_1 = self.snake_2
+                self.snake_2 = None
+
+                if not self.food and self.snake_1:
+                    self.snake_1.activate()
+            else:
+                raise Exception()
+
+    def add_food(self):
+        self.food = True
+
+    def remove_food(self):
+        with self.lock:
+            self.food = False
+            if self.snake_1:
+                self.snake_1.activate()
+
+    def space_to_reserve(self):
+        with self.lock:
+            return not self.snake_1
+
+    def reserve_spot(self, snake):
+        with self.lock:
+            if self.snake_1:
+                self.snake_2 = snake
+            else:
+                self.snake_1 = snake
+
+class SnakeState(Enum):
+    WAITING = 0
+    SPAWNING = 1
+    PLAYING = 2
+
+class Snake:
+    COLORS = [
+        (255, 255, 0),
+        (0, 0, 255),
+        (0, 255, 100),
+        ]
+
+    def __init__(self, game, snake_id, consumer):
+        consumer.set_snake(self)
+
+        self.color = self.COLORS[snake_id]
+
+        self.snake_id = snake_id
+        self.game = game
+        self.spawn_time = 2
+        self.state = SnakeState.SPAWNING
+        self.waiting_tiles = 0
+        self.cur_t = 0
+        self.tiles = []
+        self.MOVE_TIME = 0.3
+
+        self.direction = Direction.EAST
+        self.consumer = consumer
+        self.score = 0
+
+        self.lock = threading.RLock()
+
+        self.get_on_map()
+
+    def cur_pos(self):
+        return self.tiles[-1]
+
+    def get_on_map(self):
+        start_y = self.snake_id
+        start_x = 0
+        start_length = 3
+
+        for i in range(start_length):
+            x = start_x + i
+            y = start_y
+            tile = self.game.at(x, y)
+
+            if tile.occupied():
+                self.state = SnakeState.WAITING
+                self.waiting_tiles += 1
+
+
+            tile.reserve_spot(self)
+
+            self.tiles.append((x, y))
+
+    def activate(self):
+        with self.lock:
+            if self.state is SnakeState.WAITING:
+                self.waiting_tiles -= 1
+                if self.waiting_tiles == 0:
+                    self.state = SnakeState.SPAWNING
+
+    def move(self):
+        self.process_moves()
+        direction = self.direction
+
+        dx = 0
+        dy = 0
+        if direction is Direction.NORTH:
+            dy = -1
+        elif direction is Direction.SOUTH:
+            dy = 1
+        elif direction is Direction.WEST:
+            dx = -1
+        elif direction is Direction.EAST:
+            dx = 1
+
+        x, y = self.cur_pos()
+        nx = (x + dx) % self.game.WIDTH
+        ny = (y + dy) % self.game.HEIGHT
+
+        next_tile = self.game.at(nx, ny)
+
+        if next_tile.occupied():
+            return True
+        else:
+            self.game.at(nx, ny).move_to_spot(self)
+
+            self.tiles.append((nx, ny))
+
+            if next_tile.food:
+                self.increment_score()
+                self.game.add_food()
+                next_tile.remove_food()
+            else:
+                ox, oy = self.tiles.pop(0)
+                self.game.at(ox, oy).leave_spot(self)
+
+    def tick(self, delta):
+        with self.lock:
+            if self.state is SnakeState.WAITING:
+                pass
+            elif self.state is SnakeState.SPAWNING:
+                self.spawn_time -= delta
+                if self.spawn_time < 0:
+                    self.state = SnakeState.PLAYING
+            elif self.state is SnakeState.PLAYING:
+                self.play(delta)
+            else:
+                raise Exception("Unknown SnakeState")
+
+    def disconnect(self):
+        with self.lock:
+            for (x, y) in self.tiles:
+                self.game.at(x, y).leave_spot(self)
+
+            self.game.remove_snake(self)
+
+            self.consumer.game_over()
+
+    def increment_score(self):
+        self.score += 1
+        self.consumer.send_json({ "score": self.score })
+
+    def discard_moves(self):
+        while not self.consumer.input_queue.empty():
+            next_dir = self.consumer.input_queue.queue[0]
+            if self.direction.is_irrelevant(next_dir):
+                self.consumer.input_queue.get()
+            else:
+                return
+
+    def process_moves(self):
+        self.discard_moves()
+
+        if not self.consumer.input_queue.empty():
+            self.direction = self.consumer.input_queue.get()
+
+
+    def play(self, delta):
+        self.cur_t += delta
+
+        if self.cur_t >= self.MOVE_TIME:
+            self.cur_t = 0
+            try:
+                game_over = self.move()
+
+                if game_over:
+                    self.disconnect()
+
+            except Exception as e:
+                print(e)
+
+
+class SnakeGame(Disp):
+    WIDTH = 15
+    HEIGHT = 10
+    MAX_SNAKES = 3
+
+    def __init__(self):
+        self.lock = threading.RLock()
+        self.grid = [ [ Tile() for x in range(self.WIDTH) ]
+                      for y in range(self.HEIGHT) ]
+
+        self.snakes = []
+        self.waiting = []
+        self.add_food()
+
+        self.cur_t = 0
+
+    def at(self, x, y):
+        return self.grid[y][x]
+
+    def connect_snake(self, consumer):
+        with self.lock:
+            if len(self.snakes) < self.MAX_SNAKES:
+                self.add_snake(consumer)
+            else:
+                self.waiting.append(consumer)
+
+    def add_snake(self, consumer):
+        snake = Snake(self, len(self.snakes), consumer)
+        self.snakes.append(snake)
+
+    def remove_snake(self, snake):
+        with self.lock:
+            self.snakes.remove(snake)
+
+            if len(self.waiting) > 0:
+                next_consumer = self.waiting.pop(0)
+                self.connect_snake(next_consumer)
+
+    def add_food(self):
+        empties = 0
+        for i in range(self.WIDTH):
+            for j in range(self.HEIGHT):
+                if self.at(i, j).completely_empty():
+                    empties += 1
+
+        index = random.randrange(empties)
+
+        for i in range(self.WIDTH):
+            for j in range(self.HEIGHT):
+                if self.at(i, j).completely_empty():
+                    index -= 1
+                    if index == 0:
+                        self.at(i, j).add_food()
+
+    def tick(self, ctrl, delta):
+        self.cur_t += delta
+
+        for snake in self.snakes:
+            snake.tick( delta)
+
+    def render(self, ctrl):
+        ctrl.fill(0, 0, 0)
+
+        for i in range(self.WIDTH):
+            for j in range(self.HEIGHT):
+                tile = self.at(i, j)
+                if tile.snake_1:
+                    snake = tile.snake_1
+                    if snake.state is SnakeState.PLAYING:
+                        ctrl.set_pixel(i, j,
+                                       snake.color[0],
+                                       snake.color[1],
+                                       snake.color[2])
+                    else:
+                        period = 0.5
+                        mod = abs(((self.cur_t % period) / period) - 0.5)
+                        ctrl.set_pixel(i, j,
+                                       snake.color[0] * mod,
+                                       snake.color[1] * mod,
+                                       snake.color[2] * mod)
+
+                elif tile.food:
+                    ctrl.set_pixel(i, j, 255, 255, 255)
+
+        ctrl.render()
+                    
--- snake/snake_consumer.py
+++ snake/snake_consumer.py
@@ -1,28 +1,60 @@
 from channels.generic.websocket import JsonWebsocketConsumer
 
 from django.apps import apps
-from snake.snake import SnakeGameWrapper, Direction
+from snake.snake_2 import SnakeGame, Direction
 
 import queue
+import logging
 
-snake_wrapper = None
+logger = logging.getLogger(__name__)
+snake_game = None
 
 class SnakeConsumer(JsonWebsocketConsumer):
     def connect(self):
-        global snake_wrapper
-        snake_wrapper = SnakeGameWrapper(self)
-
-        ilcon = apps.get_app_config('ilcon').ilcon
-        ilcon.send(snake_wrapper)
-
         self.input_queue = queue.Queue()
+        self.snake = None
 
         self.accept()
 
+
     def disconnect(self, close_code):
-        pass
+        if self.snake:
+            self.snake.disconnect()
+
+    def set_snake(self, snake):
+        self.snake = snake
 
     def receive_json(self, content):
+        if content["msg"] == "dir":
+            self.receive_direction(content)
+        elif content["msg"] == "new":
+            self.receive_new_game(content)
+        else:
+            logger.error("Unrecognized message from client")
+
+            
+    def initialize_snake_game(self):
+        global snake_game
+        snake_game = SnakeGame()
+
+    def game_over(self):
+        self.send_json({ "msg": "game_over" })
+        self.snake = None
+        self.close()
+
+    def receive_new_game(self, content):
+        global snake_game
+
+        if not snake_game:
+            self.initialize_snake_game()
+
+        ilcon = apps.get_app_config('ilcon').ilcon
+        ilcon.send(snake_game)
+
+        snake_game.connect_snake(self)
+
+
+    def receive_direction(self, content):
         direction = content['direction']
 
         if direction == "n":
@@ -36,4 +68,5 @@ class SnakeConsumer(JsonWebsocketConsumer):
         else:
             return
 
-        self.input_queue.put(direction)
+        if self.input_queue.qsize() < 20:
+            self.input_queue.put(direction)
